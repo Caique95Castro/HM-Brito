741659ef3a07e4230ab5cdec21f0da3e1294abbf
Caique95Castro <124168022+Caique95Castro@users.noreply.github.com>
2024-06-04 15:58:03 -0300

    04-06

--- src/components/FooterHM.vue
+++ src/components/FooterHM.vue
@@ -7,11 +7,15 @@
       </div>
       <div class="institucional-footer">
         <div class="content-footer-1">
-            <h4 class="logo-footer-1"><strong>Institucional.</strong></h4>
-            <a class="link-menu" href="/">Home</a>
-            <a class="link-menu" href="/about">About</a>
-            <a class="link-menu" href="/servicos">Serviços</a>
-            <a class="link-menu" href="/contato">Contato</a>
+          <h4 class="logo-footer-1"><strong>Institucional.</strong></h4>
+          
+          <router-link class="link-menu" to="/">Home</router-link>
+          <router-link class="link-menu" to="/about">About</router-link>
+          <router-link class="link-menu" to="/aluguel"
+            >Aluguel de Caçamba</router-link
+          >
+          <router-link class="link-menu" to="/servicos">Serviços</router-link>
+          <router-link class="link-menu" to="/contato">Contato</router-link>
         </div>
       </div>
       <div class="institucional-footer">
@@ -20,19 +24,25 @@
           <a class="link-menu" href="">Aluguel de Caçamba</a>
         </div>
         <div class="content-footer-1">
-            <h4 class="logo-footer-1"><strong>Fale Conosco.</strong></h4>
-            <a class="link-menu" href="#">1199999-9999</a>
-            <a class="link-menu" href="3">seuemail@aqui.com</a>
+          <h4 class="logo-footer-1"><strong>Fale Conosco.</strong></h4>
+          <a class="link-menu" href="#">1199999-9999</a>
+          <a class="link-menu" href="3">seuemail@aqui.com</a>
         </div>
       </div>
       <div class="institucional-footer">
         <div class="content-footer-1">
-            <h4 class="logo-footer-1"><strong>Siga o nosso Instagram</strong></h4>
-            <a class="link-menu" href=""><ion-icon class="image-icons" name="logo-instagram"></ion-icon></a>
+          <h4 class="logo-footer-1"><strong>Siga o nosso Instagram</strong></h4>
+          <a class="link-menu" href=""
+            ><ion-icon class="image-icons" name="logo-instagram"></ion-icon
+          ></a>
         </div>
         <div class="content-footer-1">
-            <h4 class="logo-footer-1"><strong>Principais Locais de Atendimentos</strong></h4>
-            <p class="locais-color">COTIA, BARUERI, ALPHAVILLE, OSASCO, CARAPICUIBA</p>
+          <h4 class="logo-footer-1">
+            <strong>Principais Locais de Atendimentos</strong>
+          </h4>
+          <p class="locais-color">
+            COTIA, BARUERI, ALPHAVILLE, OSASCO, CARAPICUIBA
+          </p>
         </div>
       </div>
     </div>
