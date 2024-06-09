8178710651069ff9d50b7d340462b437644d0b56
Caique95Castro <124168022+Caique95Castro@users.noreply.github.com>
2024-06-09 16:42:13 -0300

    09-06

--- src/components/ContatoHM.vue
+++ src/components/ContatoHM.vue
@@ -14,43 +14,49 @@
           </nav>
         </div>
       </div>
-      <div class="content-about-home">
-        <div class="conteudo-tit-sub">
-          <div class="container-conteudo">
-            <h1 class="enfase-titulo"><strong>ALUGUEL DE CAÇAMBAS</strong></h1>
-            <h4 class="enfase-titulo">
-              Aquele entulho ou lixo que era um estorvo em sua obra não é
-              problema com a <strong>HMN Terraplenagem</strong>. Peça já uma de
-              nossas caçambas.
-            </h4>
-          </div>
-        </div>
-        <div class="container-about-home">
-          <div class="content-image-about-home">
-            <img class="image-about" src="../assets/img/CACAMBA1.png" alt="" />
-          </div>
-          <div class="content-text-about-home">
-            <p>
-              Com foco apenas nas regiões de
-              <strong>Cotia, Granja Viana e Vargem Grande Paulista</strong> a
-              Pirâmide Coleta atua há mais 20 anos oferecendo rapidez e
-              eficiência no aluguel de caçambas, coleta e remoção de entulho,
-              poda, gesso, isopor, plástico, papelão entre outros, em áreas
-              residenciais, comerciais e industriais.s
+      <div class="container-geral-contato">
+        <div class="content-contato">
+          <div class="box-texto-contato">
+            <h5 class="titulo-1"><strong>CONVERSE CONOSCO</strong></h5>
+            <h2 class="titulo-2"><strong>Tem alguma dúvida ou objeção em mente?</strong></h2>
+            <p class="titulo-3">
+              Nós temos uma equipe de suporte que fica 24 horas disposto a tirar
+              todas as suas dúvidas, objeções e medos em relação aos
+              tratamentos.
             </p>
-            <a class="btn-mais" href="/about">Saiba mais ></a>
+            <div class="faixa-contato"></div>
+            <a class="titulo-4" href="http://" target="_blank" rel="noopener noreferrer"><strong>(11) 99999-9999</strong></a>
+            <a class="titulo-4" href="http://" target="_blank" rel="noopener noreferrer"><strong>seuemail@aqui.com</strong></a>
+            <a
+              class="btn-link"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              ><button class="btn-link">Solicite um Orçamento</button></a
+            >
+          </div>
+          <div class="box-image-google">
+            <iframe
+            class="google-maps"
+              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d116980.99943220378!2d-46.75580311205668!3d-23.61665870985605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1svila%20lobos!5e0!3m2!1spt-BR!2sbr!4v1717960989495!5m2!1spt-BR!2sbr"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
       </div>
+
       <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-    />
-    <a href="#" class="float" target="_blank">
-      <i class="fa fa-whatsapp my-float"></i>
-    </a>
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
+      />
+      <a href="#" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+      </a>
     </section>
-   
+
     <FooterHM />
   </div>
 </template>
